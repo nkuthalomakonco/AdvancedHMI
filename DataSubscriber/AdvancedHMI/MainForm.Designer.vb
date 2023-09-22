cdfdb2bed282ac8d31d319ac114c0d0116703d45
Nkuthalo Makonco <118244106+nkuthalomakonco@users.noreply.github.com>
2023-09-22 14:16:40 +0200

    Add files via upload

---
+++ DataSubscriber/AdvancedHMI/MainForm.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    '   <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    ' <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ForceItemsIntoToolBox1 = New MfgControl.AdvancedHMI.Drivers.ForceItemsIntoToolbox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label_Bool = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label_INT = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label_Real = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.EthernetIPforCLXCom1 = New AdvancedHMIDrivers.EthernetIPforCLXCom(Me.components)
+        Me.DataSubscriber21 = New AdvancedHMIControls.DataSubscriber2(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EthernetIPforCLXCom1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataSubscriber21, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(209, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(216, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "DataSubscriber"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.LinkLabel1.Location = New System.Drawing.Point(146, 103)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(396, 13)
+        Me.LinkLabel1.TabIndex = 1
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "https://advancedhmi.com/documentation/index.php/DataSubscriber"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.GroupBox1.Controls.Add(Me.Label_Bool)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.GroupBox1.Location = New System.Drawing.Point(49, 165)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(162, 84)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "BOOL"
+        '
+        'Label_Bool
+        '
+        Me.Label_Bool.AutoSize = True
+        Me.Label_Bool.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_Bool.Location = New System.Drawing.Point(39, 33)
+        Me.Label_Bool.Name = "Label_Bool"
+        Me.Label_Bool.Size = New System.Drawing.Size(81, 25)
+        Me.Label_Bool.TabIndex = 0
+        Me.Label_Bool.Text = "FALSE"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.GroupBox2.Controls.Add(Me.Label_INT)
+        Me.GroupBox2.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.GroupBox2.Location = New System.Drawing.Point(244, 165)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(162, 84)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "INT"
+        '
+        'Label_INT
+        '
+        Me.Label_INT.AutoSize = True
+        Me.Label_INT.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_INT.Location = New System.Drawing.Point(49, 27)
+        Me.Label_INT.Name = "Label_INT"
+        Me.Label_INT.Size = New System.Drawing.Size(30, 31)
+        Me.Label_INT.TabIndex = 0
+        Me.Label_INT.Text = "0"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.GroupBox3.Controls.Add(Me.Label_Real)
+        Me.GroupBox3.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.GroupBox3.Location = New System.Drawing.Point(430, 165)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(162, 84)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "REAL"
+        '
+        'Label_Real
+        '
+        Me.Label_Real.AutoSize = True
+        Me.Label_Real.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_Real.Location = New System.Drawing.Point(34, 27)
+        Me.Label_Real.Name = "Label_Real"
+        Me.Label_Real.Size = New System.Drawing.Size(42, 25)
+        Me.Label_Real.TabIndex = 1
+        Me.Label_Real.Text = "0.0"
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.DimGray
+        Me.Button1.Location = New System.Drawing.Point(258, 301)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(135, 66)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Reset"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.NumericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.NumericUpDown1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NumericUpDown1.Location = New System.Drawing.Point(456, 320)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {2500, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(121, 30)
+        Me.NumericUpDown1.TabIndex = 3
+        '
+        'EthernetIPforCLXCom1
+        '
+        Me.EthernetIPforCLXCom1.CIPConnectionSize = 508
+        Me.EthernetIPforCLXCom1.DisableMultiServiceRequest = False
+        Me.EthernetIPforCLXCom1.DisableSubscriptions = False
+        Me.EthernetIPforCLXCom1.IniFileName = ""
+        Me.EthernetIPforCLXCom1.IniFileSection = Nothing
+        Me.EthernetIPforCLXCom1.IPAddress = "10.3.0.224"
+        Me.EthernetIPforCLXCom1.PollRateOverride = 500
+        Me.EthernetIPforCLXCom1.Port = 44818
+        Me.EthernetIPforCLXCom1.ProcessorSlot = 0
+        Me.EthernetIPforCLXCom1.Timeout = 4000
+        '
+        'DataSubscriber21
+        '
+        Me.DataSubscriber21.ComComponent = Me.EthernetIPforCLXCom1
+        Me.DataSubscriber21.PollRate = 250
+        Me.DataSubscriber21.SynchronizingObject = Me
+        Me.DataSubscriber21.Value = Nothing
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(402, 320)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 31)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "To"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 380)
+        Me.RichTextBox1.MaxLength = 214748000
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(624, 48)
+        Me.RichTextBox1.TabIndex = 5
+        Me.RichTextBox1.Text = ""
+        '
+        'MainForm
+        '
+        Me.AutoScroll = True
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(648, 440)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.KeyPreview = True
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "AdvancedHMI v3.99"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EthernetIPforCLXCom1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataSubscriber21, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DF1ComWF1 As AdvancedHMIDrivers.SerialDF1forSLCMicroCom
+    Friend WithEvents ForceItemsIntoToolBox1 As MfgControl.AdvancedHMI.Drivers.ForceItemsIntoToolbox
+    Friend WithEvents EthernetIPforCLXCom1 As AdvancedHMIDrivers.EthernetIPforCLXCom
+    Friend WithEvents DataSubscriber21 As AdvancedHMIControls.DataSubscriber2
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label_INT As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label_Bool As Label
+    Friend WithEvents Label_Real As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ DataSubscriber/AdvancedHMI/MainForm.vb
@@ -0,0 +1,114 @@
+﻿Imports System.Reflection
+Imports System.Text.RegularExpressions
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms
+
+Public Class MainForm
+    '*******************************************************************************
+    '* Stop polling when the form is not visible in order to reduce communications
+    '* Copy this section of code to every new form created
+    '*******************************************************************************
+    Private NotFirstShow As Boolean
+    Private Sub Form_VisibleChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.VisibleChanged
+        '* Do not start comms on first show in case it was set to disable in design mode
+        If NotFirstShow Then
+            AdvancedHMIDrivers.Utilities.StopComsOnHidden(components, Me)
+        Else
+            NotFirstShow = True
+        End If
+    End Sub
+#Region "PLC_TAGS"
+    'List of PLC TAGS
+    Private PLC_TAGS_BOOL As New List(Of AdvancedHMI.Drivers.PLCAddressItem)
+    Private PLC_TAGS_INTERGER As New List(Of AdvancedHMI.Drivers.PLCAddressItem)
+    Private PLC_TAGS_FLOAT As New List(Of AdvancedHMI.Drivers.PLCAddressItem)
+
+    Private PLC_TAGS As New List(Of AdvancedHMI.Drivers.PLCAddressItem)
+
+#End Region
+    Private TagsFromFile As String() = New String() {}
+    Private fileName As String = My.Computer.FileSystem.SpecialDirectories.Desktop + "\tags.txt"
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        RichTextBox1.AppendText($"[{Date.Now.ToString("G")}]:MainForm_Load.{vbLf}")
+        'Get tag names from text file.
+        If System.IO.File.Exists(fileName) Then
+            TagsFromFile = System.IO.File.ReadAllLines(fileName)
+        End If
+        'Check if file has tags.
+        If TagsFromFile.Length > 0 Then
+            For Each line As String In TagsFromFile
+                RichTextBox1.AppendText($"[{Date.Now.ToString("G")}]:Tag from file:{line}{vbNewLine}")
+                PLC_TAGS.Add(New Drivers.PLCAddressItem(line))
+            Next line
+        End If
+#Region "Add plc tags"
+        PLC_TAGS.Add(New Drivers.PLCAddressItem("AdvancedHMI_BOOL"))
+        PLC_TAGS.Add(New Drivers.PLCAddressItem("AdvancedHMI_BOOL_Reset"))
+        PLC_TAGS.Add(New Drivers.PLCAddressItem("AdvancedHMI_BOOL_Reset_Value"))
+        PLC_TAGS.Add(New Drivers.PLCAddressItem("AdvancedHMI_INT"))
+        PLC_TAGS.Add(New Drivers.PLCAddressItem("AdvancedHMI_REAL"))
+#Region "Bind the PLCAddressItems to the Subscribers"
+        For Each Item In PLC_TAGS
+            DataSubscriber21.PLCAddressValueItems.Add(Item)
+        Next Item
+#End Region
+#End Region
+    End Sub
+#Region "DataChanged Handles"
+    Private Sub DataSubscriber21_DataChanged(sender As Object, e As Drivers.Common.PlcComEventArgs) Handles DataSubscriber21.DataChanged
+        If e.ErrorId > 0 Then
+            ' MsgBox($"{sender.ToString}:{e.Values}")
+            RichTextBox1.AppendText($"{sender.ToString}:{e.Values}{vbLf}")
+        End If
+        If e.ErrorId = 0 Then
+            If e.Values IsNot Nothing AndAlso e.Values.Count > 0 Then
+                If e.PlcAddress = "AdvancedHMI_BOOL" Then
+                    Label_Bool.Text = e.Values(0)
+                ElseIf e.PlcAddress = "AdvancedHMI_REAL" Then
+                    UpDateValue(e.Values(0))
+                ElseIf e.PlcAddress = "AdvancedHMI_BOOL_Reset_Value" Then
+
+                ElseIf e.PlcAddress = "AdvancedHMI_BOOL_Reset" Then
+                    If e.Values(0) Is "True" Then
+                        Try
+                            EthernetIPforCLXCom1.Write("AdvancedHMI_BOOL_Reset_Value", NumericUpDown1.Value)
+                            EthernetIPforCLXCom1.Write("AdvancedHMI_BOOL_Reset", 0)
+                        Catch ex As Exception
+                            RichTextBox1.AppendText($"[{Date.Now.ToString("G")}]:{ex.Source}:{ex.Message}")
+                        End Try
+                    End If
+                ElseIf e.PlcAddress = "AdvancedHMI_INT" Then
+                    Label_INT.Text = e.Values(0)
+                End If
+            End If
+        End If
+    End Sub
+#End Region
+#Region "sub/function"
+    Private Sub UpDateValue(ByVal val As String)
+        '
+        Dim m As Match = Regex.Match(val, "\d{1,4}.\d{1,3}", RegexOptions.Singleline)
+        If m.Success Then
+            Label_Real.Text = $"{m.Value}"
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        'Navigate to a URL.
+        System.Diagnostics.Process.Start("https://advancedhmi.com/documentation/index.php/DataSubscriber")
+    End Sub
+
+    Private Sub BasicDataLogger1_DataChanged(sender As Object, e As Drivers.Common.PlcComEventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            EthernetIPforCLXCom1.Write("AdvancedHMI_BOOL_Reset", 1)
+        Catch ex As Exception
+            RichTextBox1.AppendText($"[{Date.Now.ToString("G")}]:{ex.Source}:{ex.Message}")
+        End Try
+    End Sub
+#End Region
+End Class
